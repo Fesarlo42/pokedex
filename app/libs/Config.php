0af75c68776db966c0e2e6d1891087f7c5ec4bf4
Fesarlo <fesarlo@gmail.com>
2025-01-28 00:08:03 +0100

    ADD - images variables

--- app/libs/Config.php
+++ app/libs/Config.php
@@ -6,10 +6,20 @@
 class Config {
     // Database configuration
     static public $db_hostname = "localhost";
-    static public $db_name   = "pokedex";
-    static public $db_user  = "root";
-    static public $db_pass    = "";
-    static public $charset = "utf-8";
+    static public $db_name     = "pokedex";
+    static public $db_user     = "root";
+    static public $db_pass     = "";
+    static public $charset     = "utf-8";
+
+    // Images path
+    static public $images_base_path     = "web/images/";
+    static public $images_artworks_path = "web/images/artworks/";
+    static public $images_sprites_path  = "web/images/sprites/";
+    static public $images_gifs_path     = "web/images/gifs/";
+    static public $images_trainers_path = "web/images/trainers/";
+    static public $images_types_path    = "web/images/types/";
+
+    static public $default_avatar = "web/images/trainers/red.php";
 }
 
 ?>
